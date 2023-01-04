cc17a0f9c64a0c704271c893bad10fd8628edfbc
Saul Pinales <saul021002@gmail.com>
2023-01-03 20:34:29 -0400

    Show all permisions of roles

--- useful queries.sql
+++ useful queries.sql
@@ -53,3 +53,15 @@ From
   sysusers
 Where
   issqlrole = 1
+
+-- get permisions of all roles
+select  princ.name
+,       princ.type_desc
+,       perm.permission_name
+,       perm.state_desc
+,       perm.class_desc
+,       object_name(perm.major_id)
+from    sys.database_principals princ
+left join
+        sys.database_permissions perm
+on      perm.grantee_principal_id = princ.principal_id
