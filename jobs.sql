eb285a447a3926b9a74b0c29e6e1c968605334b6
Saul Pinales <saul021002@gmail.com>
2023-01-03 21:28:45 -0400

    history cleanup

--- jobs.sql
+++ jobs.sql
@@ -74,15 +74,31 @@ BEGIN
 END;
 GO
 
+DROP PROCEDURE IF EXISTS dbo.spMaintanceCleanBackpupHistory
+GO
+CREATE PROCEDURE dbo.spMaintanceCleanBackpupHistory
+AS
+BEGIN
+    DECLARE @Date datetime = DATEADD(dd, -28, GETDATE());
+    EXEC msdb.dbo.sp_delete_backuphistory @oldest_date = @Date;
+END;
+GO
+
+DROP PROCEDURE IF EXISTS dbo.spMaintanceCleanJobHistory
+GO
+CREATE PROCEDURE dbo.spMaintanceCleanJobHistory
+AS
+BEGIN
+    EXEC msdb.dbo.sp_purge_jobhistory;
+END;
+GO
 /*
 Full Backup Job
 */
-USE msdb;
-GO
-EXEC dbo.sp_add_job
+EXEC msdb.dbo.sp_add_job
     @job_name = N'Weekly Star Airlines Data Backup' ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Weekly Star Airlines Data Backup',
     @step_name = N'Set database to read only',
     @subsystem = N'TSQL',
@@ -90,7 +106,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Weekly Star Airlines Data Backup',
     @step_name = N'Full Database backup',
     @subsystem = N'TSQL',
@@ -98,7 +114,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Weekly Star Airlines Data Backup',
     @step_name = N'Delete old database backups',
     @subsystem = N'TSQL',
@@ -106,7 +122,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Weekly Star Airlines Data Backup',
     @step_name = N'Set database to read write',
     @subsystem = N'TSQL',
@@ -114,31 +130,27 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC dbo.sp_add_schedule
+EXEC msdb.dbo.sp_add_schedule
     @schedule_name = N'RunWeekly',
     @freq_type = 8, -- semanal
     @freq_interval = 2, -- lunes
      @freq_recurrence_factor = 1,
     @active_start_time = 000000 ; -- 12PM
-USE msdb ;
-GO
-EXEC sp_attach_schedule
+EXEC msdb.dbo.sp_attach_schedule
    @job_name = N'Weekly Star Airlines Data Backup',
    @schedule_name = N'RunWeekly';
 GO
-EXEC dbo.sp_add_jobserver
+EXEC msdb.dbo.sp_add_jobserver
     @job_name = N'Weekly Star Airlines Data Backup';
 GO
 
 /*
 Diferential Backup Job
 */
-USE msdb ;
-GO
-EXEC dbo.sp_add_job
+EXEC msdb.dbo.sp_add_job
     @job_name = N'Nightly StarAirlines Diferential Data Backup' ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Nightly StarAirlines Diferential Data Backup',
     @step_name = N'Set database to read only',
     @subsystem = N'TSQL',
@@ -146,7 +158,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Nightly StarAirlines Diferential Data Backup',
     @step_name = N'Do Diferential backup',
     @subsystem = N'TSQL',
@@ -154,7 +166,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Nightly StarAirlines Diferential Data Backup',
     @step_name = N'Set database to read write mode',
     @subsystem = N'TSQL',
@@ -162,30 +174,26 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC dbo.sp_add_schedule
+EXEC msdb.dbo.sp_add_schedule
     @schedule_name = N'RunDaily',
     @freq_type = 4,
     @freq_interval = 1,
     @active_start_time = 000000 ;
-USE msdb ;
-GO
-EXEC sp_attach_schedule
+EXEC msdb.dbo.sp_attach_schedule
    @job_name = N'Nightly StarAirlines Diferential Data Backup',
    @schedule_name = N'RunDaily';
 GO
-EXEC dbo.sp_add_jobserver
+EXEC msdb.dbo.sp_add_jobserver
     @job_name = N'Nightly StarAirlines Diferential Data Backup';
 GO
 
 /*
 Log Backup Job
 */
-USE msdb ;
-GO
-EXEC dbo.sp_add_job
+EXEC msdb.dbo.sp_add_job
     @job_name = N'Star Airlines Log Backup' ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Star Airlines Log Backup',
     @step_name = N'Set database to read only',
     @subsystem = N'TSQL',
@@ -193,7 +201,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Star Airlines Log Backup',
     @step_name = N'Do log backup',
     @subsystem = N'TSQL',
@@ -201,7 +209,7 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC sp_add_jobstep
+EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'Star Airlines Log Backup',
     @step_name = N'Set database to read write only',
     @subsystem = N'TSQL',
@@ -209,20 +217,18 @@ EXEC sp_add_jobstep
     @retry_attempts = 5,
     @retry_interval = 5 ;
 GO
-EXEC dbo.sp_add_schedule
+EXEC msdb.dbo.sp_add_schedule
     @schedule_name = N'RunEvery15Minutes',
     @freq_type = 4, -- on daily basis
     @freq_interval = 1, -- don't use this one
     @freq_subday_type = 4,  -- units between each exec: minutes
     @freq_subday_interval = 15,  -- number of units between each exec
     @active_start_time = 000000 ;
-USE msdb ;
-GO
-EXEC sp_attach_schedule
+EXEC msdb.dbo.sp_attach_schedule
    @job_name = N'Star Airlines Log Backup',
    @schedule_name = N'RunEvery15Minutes';
 GO
-EXEC dbo.sp_add_jobserver
+EXEC msdb.dbo.sp_add_jobserver
     @job_name = N'Star Airlines Log Backup' ;
 GO
 
@@ -233,6 +239,8 @@ Index, rebuild index
 /*
 Statidictis
 */
+-- auto create
+-- auto update
 
 /*
 check database integrity
@@ -241,3 +249,29 @@ check database integrity
 /*
 History cleanup
 */
+EXEC msdb.dbo.sp_add_job
+    @job_name = N'History Cleanup' ;
+GO
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = N'Backups History Cleanup',
+    @step_name = N'Clear database backups history',
+    @subsystem = N'TSQL',
+    @command = N'EXEC dbo.spMaintanceCleanBackpupHistory',
+    @retry_attempts = 5,
+    @retry_interval = 5 ;
+GO
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = N'Jobs History Cleanup',
+    @step_name = N'Clear jobs history',
+    @subsystem = N'TSQL',
+    @command = N'EXEC dbo.spMaintanceCleanJobHistory',
+    @retry_attempts = 5,
+    @retry_interval = 5 ;
+GO
+EXEC msdb.dbo.sp_attach_schedule
+   @job_name = N'History Cleanup',
+   @schedule_name = N'RunWeekly';
+GO
+EXEC msdb.dbo.sp_add_jobserver
+    @job_name = N'History Cleanup';
+GO
