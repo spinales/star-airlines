f08c1d3980608dfe9b6eaa900542a87e96612c6a
Saul Pinales <saul021002@gmail.com>
2023-01-09 23:43:33 -0400

    script to erase foreign keys and relationships

--- useful queries.sql
+++ useful queries.sql
@@ -77,3 +77,30 @@ SELECT t.[text]
 FROM sys.dm_exec_cached_plans AS p
 CROSS APPLY sys.dm_exec_sql_text(p.plan_handle) AS t
 WHERE t.[text] LIKE N'%something unique about your query%';
+
+-- drop foreign keys
+while(exists(select 1 from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_TYPE='FOREIGN KEY'))
+begin
+ declare @sql nvarchar(2000)
+ SELECT TOP 1 @sql=('ALTER TABLE ' + TABLE_SCHEMA + '.[' + TABLE_NAME
+ + '] DROP CONSTRAINT [' + CONSTRAINT_NAME + ']')
+ FROM information_schema.table_constraints
+ WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
+ exec (@sql)
+ PRINT @sql
+end
+
+-- drop tables
+while(exists(select 1 from INFORMATION_SCHEMA.TABLES
+             where TABLE_NAME != '__MigrationHistory'
+             AND TABLE_TYPE = 'BASE TABLE'))
+begin
+ declare @sql nvarchar(2000)
+ SELECT TOP 1 @sql=('DROP TABLE ' + TABLE_SCHEMA + '.[' + TABLE_NAME
+ + ']')
+ FROM INFORMATION_SCHEMA.TABLES
+ WHERE TABLE_NAME != '__MigrationHistory' AND TABLE_TYPE = 'BASE TABLE'
+exec (@sql)
+ /* you dont need this line, it just shows what was executed */
+ PRINT @sql
+end
