4e4812280ca868f4566206a662ba9a630f0e1e49
Saul Pinales <saul021002@gmail.com>
2023-01-15 20:36:22 -0400

    script to merge files

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat structure.sql \
+functions.sql \
+'stored procedures.sql' \
+index.sql \
+security.sql \
+views.sql \
+CommandExecute.sql \
+DatabaseBackup.sql \
+DatabaseIntegrityCheck.sql \
+IndexOptimize.sql \
+'jobs.sql' > dbscript.sql
+
+
+
